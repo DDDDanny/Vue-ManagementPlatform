fe7888837f5b51c9d8d7411242896c856b4b3395
DDDDanny <707956456@qq.com>
2020-06-08 20:52:25 +0800

    feat: 新增退出功能（1. 清除sessionStorage中的token；2. 跳转至登录页）

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
-        Home 组件
+        <el-button type="info" @click="logout">退出</el-button>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Home"
+        methods: {
+            logout() {
+                window.sessionStorage.clear();
+                this.$router.push('/login')
+            }
+        }
     }
 </script>
 
