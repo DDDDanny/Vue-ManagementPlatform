96c5e080f07c5d4b1b95f8c875d5dca092c922f3
DDDDanny <707956456@qq.com>
2020-06-08 22:00:16 +0800

    feat: 设置请求拦截器，请求头中挂载Authorization

--- src/main.js
+++ src/main.js
@@ -9,6 +9,13 @@ import axios from 'axios'
 
 // 配置请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 设置拦截器
+axios.interceptors.request.use(config => {
+  // 请求头中挂载Authorization
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  console.log(config)
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
