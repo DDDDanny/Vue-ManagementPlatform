c02b18301e80ecf2829ba4a4d441b25e245b3f4e
DDDDanny <707956456@qq.com>
2020-06-08 21:12:16 +0800

    perf: 优化element-ui导入方式

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,7 +8,7 @@
     export default {
         methods: {
             logout() {
-                window.sessionStorage.clear();
+                window.sessionStorage.clear()
                 this.$router.push('/login')
             }
         }
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
-import { Form, FormItem } from 'element-ui'
-import { Input } from 'element-ui'
-// 导入弹框提示组件
-import { Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+
 // 挂载为Vue上的属性
 Vue.prototype.$message = Message
+
 // 注册组件
 Vue.use(Button)
 Vue.use(Form)
