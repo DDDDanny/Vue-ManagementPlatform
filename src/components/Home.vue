1547b667be27cd24c4b3a9fcac3dff5cd77755be
DDDDanny <707956456@qq.com>
2020-06-08 21:36:21 +0800

    feat: 初始化页面布局，调整Header元素

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,21 @@
 <template>
-    <div>
-        <el-button type="info" @click="logout">退出</el-button>
-    </div>
+    <el-container class="home-container">
+        <!-- 头部区域 -->
+        <el-header>
+            <div>
+                <img src="../assets/logo.png" alt="">
+                <span>Vue练习-后台管理系统</span>
+            </div>
+            <el-button type="info" @click="logout">退出</el-button>
+        </el-header>
+        <!-- 页面主体 -->
+        <el-container>
+            <!-- 侧边栏 -->
+            <el-aside width="200px">Aside</el-aside>
+            <!-- 右侧主体 -->
+            <el-main>Main</el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
@@ -16,5 +30,31 @@
 </script>
 
 <style scoped lang="less">
-
+    .home-container{
+        height: 100%;
+    }
+    .el-header {
+        background-color: #373d41;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        color: #ffffff;
+        font-size: 20px;
+        > div {
+            display: flex;
+            align-items: center;
+            span {
+                margin-left: 15px;
+            }
+            img {
+                width: 40px;
+            }
+        }
+    }
+    .el-aside {
+        background-color: #333744;
+    }
+    .el-main {
+        background-color: #EAEDF1;
+    }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main } from 'element-ui'
 
 // 挂载为Vue上的属性
 Vue.prototype.$message = Message
@@ -9,3 +9,7 @@ Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
