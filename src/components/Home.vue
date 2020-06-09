d3be534b33682828b26ae1afdae643a60d1ccd97
DDDDanny <707956456@qq.com>
2020-06-09 21:01:36 +0800

    feat: 通过路由重定向实现想Home页面Main部分添加组件

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -35,7 +35,10 @@
                 </el-menu>
             </el-aside>
             <!-- 右侧主体 -->
-            <el-main>Main</el-main>
+            <el-main>
+                <!-- 路由占位符 -->
+                <router-view></router-view>
+            </el-main>
         </el-container>
     </el-container>
 </template>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h3>Welcome</h3>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped lang="less">
+
+</style>
