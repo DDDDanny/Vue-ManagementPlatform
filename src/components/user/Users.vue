ac65390cad4c303672c326b1e2689bba2d2fa65e
DDDDanny <707956456@qq.com>
2020-06-10 23:52:42 +0800

    feat: 实现用户状态变更

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -28,7 +28,7 @@
                 <el-table-column prop="role_name" label="角色"></el-table-column>
                 <el-table-column prop="mg_state" label="状态">
                     <template slot-scope="scope">
-                        <el-switch v-model="scope.row.mg_state"></el-switch>
+                        <el-switch v-model="scope.row.mg_state" @change="userStateChanged(scope.row)"></el-switch>
                     </template>
                 </el-table-column>
                 <el-table-column label="操作" width="180px">
@@ -96,6 +96,14 @@
                 this.queryInfo.pagenum = newPage
                 this.getUserList()
 
+            },
+            async userStateChanged(userInfo) {
+                const {data: res} = await this.$http.put(`users/${userInfo.id}/state/${userInfo.mg_state}`)
+                if (res.meta.status !== 200){
+                    userInfo.mg_state = !userInfo.mg_state
+                    return this.$message.error('更新用户状态失败')
+                }
+                this.$message.success('更新用户状态成功！')
             }
         }
     }
