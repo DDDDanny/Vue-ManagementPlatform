3ba42cf8ca289ac5c1de850c105d38664cbfb8b0
DDDDanny <707956456@qq.com>
2020-06-11 00:17:12 +0800

    feat: 实现搜索功能

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -11,8 +11,8 @@
             <!-- 搜索与添加区 -->
             <el-row :gutter="20">
                 <el-col :span="8">
-                    <el-input placeholder="请输入内容">
-                        <el-button slot="append" icon="el-icon-search"></el-button>
+                    <el-input placeholder="请输入内容" v-model="queryInfo.query" clearable @clear="getUserList">
+                        <el-button slot="append" icon="el-icon-search" @click="getUserList"></el-button>
                     </el-input>
                 </el-col>
                 <el-col :span="4">
