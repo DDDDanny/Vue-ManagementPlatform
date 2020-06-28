8cdfd71cb48d867fd53b5a51fd50411698dfd344
DDDDanny <707956456@qq.com>
2020-06-28 23:52:15 +0800

    feat: 配置商品分类路由

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Welcome from "@/components/Welcome";
 import Users from "@/components/user/Users";
 import Rights from "@/components/power/Rights";
 import Roles from "@/components/power/Roles";
+import Cate from "@/components/goods/Cate";
 
 Vue.use(VueRouter)
 
@@ -21,6 +22,8 @@ const routes = [
             { path: '/users', component:Users },
             { path: '/rights', component:Rights },
             { path: '/roles', component:Roles },
+            { path: '/roles', component:Roles },
+            { path: '/categories', component:Cate },
         ],
     },
 ]
