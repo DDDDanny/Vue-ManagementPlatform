fc2fa106e66e30c346d45c32382052381402a183
DDDDanny <707956456@qq.com>
2020-07-06 20:16:52 +0800

    feat: 初始化商品列表页面基础UI结构

--- src/components/goods/Goods.vue
+++ src/components/goods/Goods.vue
@@ -1,6 +1,24 @@
 <template>
     <div>
-        商品列表页面
+        <!-- 面包屑导航 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+            <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+        </el-breadcrumb>
+        <!-- 卡片视图 -->
+        <el-card>
+            <el-row :gutter="20">
+                <el-col :span="8">
+                    <el-input placeholder="请输入内容">
+                        <el-button slot="append" icon="el-icon-search"></el-button>
+                    </el-input>
+                </el-col>
+                <el-col :span="4">
+                    <el-button type="primary">添加商品</el-button>
+                </el-col>
+            </el-row>
+        </el-card>
     </div>
 </template>
 
