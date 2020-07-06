e269153847e973a6eb57d365921f301d1a474104
DDDDanny <707956456@qq.com>
2020-07-07 01:40:31 +0800

    feat: 完成基本信息Tab栏中UI样式结构

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -20,13 +20,33 @@
                 <el-step title="完成"></el-step>
             </el-steps>
             <!-- Tab组件 -->
-            <el-tabs :tab-position="'left'" style="height: 200px;margin: 50px" v-model="activeIndex" @tab-click="handleClick">
-                <el-tab-pane label="基本信息" name="0">基本信息</el-tab-pane>
-                <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
-                <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
-                <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
-                <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
-            </el-tabs>
+            <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="100px" label-position="top">
+                <el-tabs :tab-position="'left'" style="margin: 50px" v-model="activeIndex">
+                    <el-tab-pane label="基本信息" name="0">
+                        <el-form-item label="商品名称" prop="goods_name">
+                            <el-input v-model="addForm.goods_name"></el-input>
+                        </el-form-item>
+                        <el-form-item label="商品价格" prop="goods_price">
+                            <el-input v-model="addForm.goods_price"></el-input>
+                        </el-form-item>
+                        <el-form-item label="商品重量" prop="goods_weight">
+                            <el-input v-model="addForm.goods_weight"></el-input>
+                        </el-form-item>
+                        <el-form-item label="商品数量" prop="goods_number">
+                            <el-input v-model="addForm.goods_number"></el-input>
+                        </el-form-item>
+                        <el-form-item label="商品分类" prop="goods_cat">
+                            <el-cascader :options="cateList" :props="cateProps" v-model="addForm.goods_cat"
+                                    @change="handleChange">
+                            </el-cascader>
+                        </el-form-item>
+                    </el-tab-pane>
+                    <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
+                    <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
+                    <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
+                    <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
+                </el-tabs>
+            </el-form>
         </el-card>
     </div>
 </template>
@@ -35,7 +55,56 @@
     export default {
         data() {
             return {
-                activeIndex: '0'
+                activeIndex: '0',
+                // 添加商品的表单数据对象
+                addForm: {
+                    goods_name: '',
+                    goods_price: 0,
+                    goods_weight: 0,
+                    goods_number: 0,
+                    goods_cat: []
+                },
+                addFormRules: {
+                    goods_name: [
+                        { required: true, message: '请输入商品名称', trigger: 'blur' }
+                    ],
+                    goods_price: [
+                        { required: true, message: '请输入商品价格', trigger: 'blur' }
+                    ],
+                    goods_weight: [
+                        { required: true, message: '请输入商品重量', trigger: 'blur' }
+                    ],
+                    goods_number: [
+                        { required: true, message: '请输入商品数量', trigger: 'blur' }
+                    ],
+                    goods_cat: [
+                        { required: true, message: '请选择商品分类', trigger: 'blur' }
+                    ],
+                },
+                cateList: [],
+                cateProps: {
+                    expandTrigger: 'hover',
+                    label: 'cat_name',
+                    value: 'cat_id',
+                    children: 'children'
+                }
+
+            }
+        },
+        created() {
+            this.getCateList()
+        },
+        methods: {
+            async getCateList() {
+                const {data: res} = await this.$http.get('categories')
+                if (res.meta.status !== 200) {
+                    return this.$message.error('获取分类信息失败！')
+                }
+                this.cateList = res.data
+            },
+            // 级联选择器选中变化，会触发这个函数
+            handleChange() {
+                console.log('hah')
             }
         }
     }
