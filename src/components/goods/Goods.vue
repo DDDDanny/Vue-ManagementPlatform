616d628c6eb448115cf6973968f0a15572efeb61
DDDDanny <707956456@qq.com>
2020-07-06 22:00:46 +0800

    feat: 获取商品列表数据并渲染商品表格

--- src/components/goods/Goods.vue
+++ src/components/goods/Goods.vue
@@ -18,13 +18,55 @@
                     <el-button type="primary">添加商品</el-button>
                 </el-col>
             </el-row>
+            <!-- table 表格区 -->
+            <el-table :data="goodsList" border stripe>
+                <el-table-column type="index" label="#"></el-table-column>
+                <el-table-column label="商品名称" prop="goods_name"></el-table-column>
+                <el-table-column label="商品价格(元)" prop="goods_price" width="100px"></el-table-column>
+                <el-table-column label="商品重量" prop="goods_weight" width="70px"></el-table-column>
+                <el-table-column label="商品创建时间" prop="add_time" width="140px"></el-table-column>
+                <el-table-column label="操作" width="150px">
+                    <template >
+                        <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+                        <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
         </el-card>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Goods"
+        data() {
+            return {
+                // 查询参数对象
+                queryInfo: {
+                    query: '',
+                    pagenum: 1,
+                    pagesize: 10
+                },
+                // 商品列表
+                goodsList: [],
+                // 商品总数
+                total: 0
+            }
+        },
+        created() {
+            this.getGoodsList()
+        },
+        methods: {
+            // 根据分页获取对应商品列表
+            async getGoodsList() {
+                const {data: res} = await this.$http.get('goods', { params: this.queryInfo })
+                if (res.meta.status !== 200) {
+                    return this.$message.error('获取商品列表失败')
+                }
+                this.$message.success('获取商品列表成功')
+                this.goodsList = res.data.goods
+                this.total = res.data.total
+            }
+        }
     }
 </script>
 
