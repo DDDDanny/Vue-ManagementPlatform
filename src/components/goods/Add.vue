06c7682722de2310bea1853310b98481e16caead
DDDDanny <707956456@qq.com>
2020-07-07 00:25:52 +0800

    feat: 渲染左侧Tab栏

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -19,6 +19,14 @@
                 <el-step title="商品内容"></el-step>
                 <el-step title="完成"></el-step>
             </el-steps>
+            <!-- Tab组件 -->
+            <el-tabs :tab-position="'left'" style="height: 200px;margin: 50px">
+                <el-tab-pane label="基本信息">基本信息</el-tab-pane>
+                <el-tab-pane label="商品参数">商品参数</el-tab-pane>
+                <el-tab-pane label="商品属性">商品属性</el-tab-pane>
+                <el-tab-pane label="商品图片">商品图片</el-tab-pane>
+                <el-tab-pane label="商品内容">商品内容</el-tab-pane>
+            </el-tabs>
         </el-card>
     </div>
 </template>
