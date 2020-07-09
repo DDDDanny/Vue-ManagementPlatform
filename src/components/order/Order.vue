d9144de52a2e7fe07a1a3cf8db2b36c17236d5e4
DDDDanny <707956456@qq.com>
2020-07-10 00:57:10 +0800

    feat: 完成订单页面基础UI结构

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -1,6 +1,21 @@
 <template>
     <div>
-        这是订单页面
+        <!-- 面包屑导航 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>订单管理</el-breadcrumb-item>
+            <el-breadcrumb-item>订单列表</el-breadcrumb-item>
+        </el-breadcrumb>
+        <!-- 卡片视图 -->
+        <el-card>
+            <el-row>
+                <el-col :span="8">
+                    <el-input placeholder="请输入内容">
+                        <el-button slot="append" icon="el-icon-search"></el-button>
+                    </el-input>
+                </el-col>
+            </el-row>
+        </el-card>
     </div>
 </template>
 
