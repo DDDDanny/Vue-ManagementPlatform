182c772364f80134cbb13fbc54a84c5c6b85042d
DDDDanny <707956456@qq.com>
2020-07-10 00:35:57 +0800

    feat: 完成订单页面初始化及路由配置

---
+++ src/components/order/Order.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        这是订单页面
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Order"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ import Cate from "@/components/goods/Cate";
 import Params from "@/components/goods/Params";
 import Goods from "@/components/goods/Goods";
 import Add from "@/components/goods/Add";
+import Order from "@/components/order/Order";
 
 Vue.use(VueRouter)
 
@@ -29,6 +30,7 @@ const routes = [
             { path: '/params', component:Params },
             { path: '/goods', component:Goods },
             { path: '/goods/add', component:Add },
+            { path: '/orders', component:Order },
         ],
     },
 ]
