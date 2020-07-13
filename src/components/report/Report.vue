507b95c4ead35fbb81ddabef3e6c2cf4f08f70b0
DDDDanny <707956456@qq.com>
2020-07-13 23:09:16 +0800

    feat: 初始化数据报表页面基础UI结构

---
+++ src/components/report/Report.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <!-- 面包屑导航 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+            <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+        </el-breadcrumb>
+        <!-- 卡片视图 -->
+        <el-card>
+            hah
+        </el-card>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Report"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from "@/components/goods/Params";
 import Goods from "@/components/goods/Goods";
 import Add from "@/components/goods/Add";
 import Order from "@/components/order/Order";
+import Report from "@/components/report/Report";
 
 Vue.use(VueRouter)
 
@@ -31,6 +32,7 @@ const routes = [
             { path: '/goods', component:Goods },
             { path: '/goods/add', component:Add },
             { path: '/orders', component:Order },
+            { path: '/reports', component:Report },
         ],
     },
 ]
