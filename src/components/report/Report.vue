af322ae6dca128940dfa9767c87b066a21d82d44
DDDDanny <707956456@qq.com>
2020-07-13 23:27:36 +0800

    feat: 完成数据报表统计图的渲染

--- src/components/report/Report.vue
+++ src/components/report/Report.vue
@@ -8,14 +8,66 @@
         </el-breadcrumb>
         <!-- 卡片视图 -->
         <el-card>
-            hah
+            <div id="main" style="width: 800px;height: 400px;"></div>
         </el-card>
     </div>
 </template>
 
 <script>
+    import echarts from 'echarts'
+    import _ from 'lodash'
     export default {
-        name: "Report"
+        data() {
+            return {
+                options: {
+                    title: {
+                        text: '用户来源'
+                    },
+                    tooltip: {
+                        trigger: 'axis',
+                        axisPointer: {
+                            type: 'cross',
+                            label: {
+                                backgroundColor: '#E9EEF3'
+                            }
+                        }
+                    },
+                    grid: {
+                        left: '3%',
+                        right: '4%',
+                        bottom: '3%',
+                        containLabel: true
+                    },
+                    xAxis: [
+                        {
+                            boundaryGap: false
+                        }
+                    ],
+                    yAxis: [
+                        {
+                            type: 'value'
+                        }
+                    ]
+                }
+            }
+        },
+        created() {
+
+        },
+        async mounted() {
+            var myChart = echarts.init(document.getElementById('main'))
+
+            const {data: res} = await this.$http.get('reports/type/1')
+            if (res.meta.status !== 200) {
+                return this.$message.error('获取折线图数据失败')
+            }
+            const result = _.merge(res.data, this.options)
+            // 使用刚指定的配置项和数据显示图表。
+            myChart.setOption(result);
+        },
+        methods: {
+
+        }
     }
 </script>
 
