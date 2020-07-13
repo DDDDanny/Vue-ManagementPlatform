3160b5865f2a30a679b853c7b387d1bd99c463b2
DDDDanny <707956456@qq.com>
2020-07-13 22:19:59 +0800

    feat: 订单管理完成表格渲染

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -15,6 +15,30 @@
                     </el-input>
                 </el-col>
             </el-row>
+            <!-- 订单列表数据 -->
+            <el-table :data="orderList" border stripe>
+                <el-table-column type="index" label="#"></el-table-column>
+                <el-table-column label="订单编号" prop="order_number"></el-table-column>
+                <el-table-column label="订单价格" prop="order_price"></el-table-column>
+                <el-table-column label="是否付款" prop="pay_status">
+                    <template slot-scope="scope">
+                        <el-tag type="success" v-if="scope.row.pay_status === '1'">已付款</el-tag>
+                        <el-tag type="danger" v-else>未付款</el-tag>
+                    </template>
+                </el-table-column>
+                <el-table-column label="是否发货" prop="is_send"></el-table-column>
+                <el-table-column label="下单时间" prop="create_time">
+                    <template slot-scope="scope">
+                        {{scope.row.create_time | dateFormat}}
+                    </template>
+                </el-table-column>
+                <el-table-column label="操作">
+                    <template >
+                        <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+                        <el-button type="success" icon="el-icon-location" size="mini"></el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
         </el-card>
     </div>
 </template>
