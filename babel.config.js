5da0436a0cc12055c3b0659091fc3389c684b8f6
DDDDanny <707956456@qq.com>
2020-07-14 22:13:01 +0800

    feat: 按需移除console

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
+// 项目发布阶段需要用到的babel插件
+const prodPlugins = []
+if (process.env.NODE_ENV === 'production') {
+  prodPlugins.push('transform-remove-console')
+}
+
 module.exports = {
   "presets": [
     "@vue/cli-plugin-babel/preset"
@@ -10,6 +16,6 @@ module.exports = {
         "styleLibraryName": "theme-chalk"
       }
     ],
-    'transform-remove-console'
+    ...prodPlugins
   ]
 }
